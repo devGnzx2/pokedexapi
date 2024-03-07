072d9b61a5647aeff5ea132ae5c5f00ad6f1061d
carlos gonzalez <dev.gnzx2@gmail.com>
2024-03-07 00:07:47 -0300

    add: Feature searc+scrollInfinity

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,27 +1,63 @@
 import { IconSearch } from "@tabler/icons-react"
 import axios from "axios"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import PokemonList from "./PokemonList"
+import { useIntersectionObserver } from "../hooks/useIntersectionObserver"
+
+const INITIAL_LIMIT = 40
+const INCREASE_LIMIT = 20
+
 
 const Pokemons = () => {
-  const [AllPokemons, setAllPokemons] = useState ([])
+  const [allPokemons, setAllPokemons] = useState ([])
+  const [pokemonName, setPokemonName] = useState ("")
+  const [limit, setLimit] = useState (INITIAL_LIMIT)
+
+  const targetObserver = useRef(null)
+  const entry = useIntersectionObserver(targetObserver, {})
+  const isVisible = !!entry?.isIntersecting
+
+  const pokemonByName = allPokemons.filter((pokemon) => pokemon.name.includes(pokemonName));
+
+  const handleChangePokemonName = (e) => setPokemonName(e.target.value.toLowerCase());
+
   useEffect(() => {
     axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=50")
+      .get("https://pokeapi.co/api/v2/pokemon?limit=898")
       .then(({ data }) => setAllPokemons(data.results))
       .catch((err) => console.log(err))
   },[])
+
+  useEffect(() => {
+    if(isVisible){
+      const maxPokemons = pokemonByName.length
+      const newLimit = limit + INCREASE_LIMIT
+      newLimit > maxPokemons ? setLimit(maxPokemons) :
+      setLimit(newLimit)
+    } 
+  },[isVisible])
+
+  useEffect(() => {
+    setLimit(INITIAL_LIMIT)
+  }, [pokemonName])
   return (
     <section className="p-4 PY-5">
-      <form action="">
+      <form>
         <div className="bg-white p-4 flex rounded-2xl text-lg">
-          <input className="outline-none flex-1" type="text" placeholder="Search your pokemon"/>
+          <input className="outline-none flex-1" 
+                type="text" 
+                autoComplete="off" 
+                placeholder="Search your pokemon" 
+                name="pokemonName" 
+                onChange={handleChangePokemonName}/>
           <button className="bg-red-500 p-2 rounded-xl shadow-lg shadow-red-500/20 hover:bg-red-400 transition-colors">
             <IconSearch color="white" stroke={3}/>
           </button>
         </div>
       </form>
-      <PokemonList pokemons={AllPokemons}/>
+      <PokemonList pokemons={pokemonByName.slice(0, limit)}/>
+      {/*observer*/}
+      <span ref={targetObserver}></span>
     </section>
   )
 }
